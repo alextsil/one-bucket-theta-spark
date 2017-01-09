bf81562d9fb84794a04bed901685b92d7454a3e3
alextsil <alex19924@gmail.com>
2017-01-09 15:43:41 +0200

    pc transfer

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "one-bucket-theta-spark"
 
-version := "1.0"
+version := "0.6"
 
 scalaVersion := "2.11.6"
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,49 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
+  val logger: Logger = Logger.getLogger(this.getClass)
+
   def main(args: Array[String]): Unit = {
     //init
     val conf = new SparkConf()
       .setAppName("one-bucket-theta-spark")
-
     val sc = SparkContext.getOrCreate(conf)
     val spark = SparkSession.builder().getOrCreate
 
-    //data loading --
-    val storeDF = spark.read.parquet("/media/alextsil/sparkPerfData/tpcds/store/part-r-00000-247e1f26-c381-44d9-b91a-3698745e51b8.snappy.parquet")
-    val caDF = spark.read.parquet("/media/alextsil/sparkPerfData/tpcds/customer_address/part-r-00000-7906df48-30c1-46ee-921a-a56ef3ba34b4.snappy.parquet")
-
+    //Load (tpcds generated) data
+    val storeDF = spark.read.parquet(args(1))
+    val caDF = spark.read.parquet(args(2))
     //DF to RDD
     val storesRdd = storeDF.rdd
     val caRdd = caDF.rdd
+    val workerCount: Integer = args(0).toInt
 
-    //Map using zip as key
-    val mappedStores = storesRdd.map(storeRow => (storeRow.get(25), storeRow))
-    val mappedCa = caRdd.map(caRow => (caRow.get(9), caRow))
+    if (this.isTheorem2Case(storesRdd, caRdd, workerCount)) {
+      //Map using zip as key
+      val mappedStores = storesRdd.map(storeRow => (storeRow.get(25), storeRow))
+      val mappedCa = caRdd.map(caRow => (caRow.get(9), caRow))
 
-    val partitionedCa = mappedCa.partitionBy(new RandomPartitioner(args(0).toInt))
-    partitionedCa.persist.collect
-    partitionedCa.join(mappedStores).collect()
+      val partitionedCa = mappedCa.partitionBy(new RandomPartitioner(workerCount))
+      partitionedCa.persist
+      partitionedCa.join(mappedStores).collect() //kane to collect na grafei se file(?)
+    }
 
-    readChar()
+    readChar() //Pause
+  }
+
+  // |S| < |T|/r
+  def isTheorem2Case(s: RDD[Row], t: RDD[Row], r: Integer): Boolean = {
+    val sSize: Integer = s.countApprox(7000, 0.95).initialValue.mean.toInt//todo: check it akrivws kanei to initialValue
+    val tSize: Integer = t.countApprox(7000, 0.95).initialValue.mean.toInt
+
+    if (sSize > tSize) {
+
+    }
+
+    this.logger.info("Theorem 2 case detected")
+    true; //todo : grapse tous kanones kai run sto clusteraki ^_^
   }
 }
