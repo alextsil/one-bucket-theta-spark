f86fe9d745998d1b01f4313fe1cee0ca576a9b99
alextsil <alex19924@gmail.com>
2017-01-11 01:17:15 +0200

    final - me full tuple output

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
   val logger: Logger = Logger.getLogger(this.getClass)
+  val pathToExportResults = "/media/joinOutput/out"
 
   def main(args: Array[String]): Unit = {
     //init
@@ -12,42 +13,40 @@ object Main {
     val sc = SparkContext.getOrCreate(conf)
     val spark = SparkSession.builder().getOrCreate
 
-    //Load (tpcds generated) data
+    //Load data
     val storeDF = spark.read.parquet(args(1))
     val caDF = spark.read.parquet(args(2))
+
     //DF to RDD
     val storesRdd = storeDF.rdd
     val caRdd = caDF.rdd
     val workerCount: Integer = args(0).toInt
 
     if (this.isTheorem2Case(storesRdd, caRdd, workerCount)) {
-      //Map using zip as key
-      val mappedStores = storesRdd.map(storeRow => (storeRow.get(25), storeRow))
-      val mappedCa = caRdd.map(caRow => (caRow.get(9), caRow))
-
+      val mappedCa = caRdd.map(caRow => (caRow.get(0), caRow))
       val partitionedCa = mappedCa.partitionBy(new RandomPartitioner(workerCount))
-      //partitionedCa.persist //Gia na mporw na dw to partitioning sto UI
 
-      //"joinOutput" folder must exist and have -777 for all new children folders
-      val pathToExport = "/media/joinOutput/out"
+      //Anagkastika map gia na mporesw na xrhsimopoihsw ton RandomPartitioner
+      val cartesian = partitionedCa.cartesian(storesRdd)
 
-      val cartesian = caRdd.cartesian(storesRdd)
+      //Epanafora se morfh [store, customer_address]
+      val properCartesian = cartesian.map(c => (c._1._2, c._2))
 
-      val filteredCartesian = cartesian
+      val filteredCartesian = properCartesian
         .filter(c => c._1.getAs("ca_zip") != null)
         .filter(c => !(c._1.getAs("ca_zip").asInstanceOf[String].substring(0, 4) == c._2.getAs("s_zip").asInstanceOf[String].substring(0, 4)))
 
-      logger.info("filter cartesian count : " + filteredCartesian.count())
+      //      logger.info("filter cartesian count : " + filteredCartesian.count())
 
-      //Try to save output
+      //Try to save results to file
       try {
-//        filteredCartesian.saveAsTextFile("/media/joinOutput/out1")
-        this.logger.info("Saved join result tuples to : " + pathToExport)
+        filteredCartesian.saveAsTextFile(pathToExportResults)
+        this.logger.info("Saved theta join result tuples to : " + pathToExportResults)
       } catch {
         case e: Exception => this.logger.error("Couldn't save file. Message is : " + e.getMessage)
       }
     } else {
-      this.logger.error("Input data dont match theorem 2 case. Aborting launch...")
+      this.logger.error("Input data dont match theorem 2 case. Aborting execution...")
     }
 
     readChar() //Pauses execution to allow for inspection
