53a2c90e39b9b67e12f27bebb6ddcb55b790e0ad
alextsil <alex19924@gmail.com>
2017-01-22 16:28:57 +0200

    added logging pantou

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -35,32 +35,48 @@ object Main {
     //Shuffle tis listes gia na einai random ta tuples otan parw range apo kathe lista
     val tTuples = Random.shuffle(storesRdd.collect().toList)
     val sTuples = Random.shuffle(caRdd.collect().toList)
+    logger.info("tTuples count after random: " + tTuples.size + "sTuples count after random: " + sTuples.size)
 
     val hLoops = (storesCount / dimensionSize).toInt
     val vLoops = (caCount / dimensionSize).toInt
-    var region = 1
+    logger.info("hLoops: " + hLoops + "vLoops: " + vLoops)
 
     var regionalJoinObjects = new ListBuffer[RegionalJoin]
 
+    var region = 0
     for (i <- 1 to vLoops) {
-      var verticalList = sTuples.drop(dimensionSize * (i - 1)).take((dimensionSize * i) - 1)
+      logger.info("i value: " + i)
+      val verticalList = sTuples.drop(dimensionSize * (i - 1)).take((dimensionSize * i) - 1)
+      logger.info("vertical list size: " + verticalList.size)
+      region += 1
       for (j <- 1 to hLoops) {
-        var horizontalList = tTuples.drop(dimensionSize * (j - 1)).take((dimensionSize * j) - 1)
-        regionalJoinObjects += RegionalJoin(region, horizontalList, verticalList)
+        logger.info("j value: " + j)
+        val horizontalList = tTuples.drop(dimensionSize * (j - 1)).take((dimensionSize * j) - 1)
+        logger.info("horizontal list size: " + horizontalList.size)
+
+        val rj = RegionalJoin(region, horizontalList, verticalList)
+        logger.info("regional join obj created: " + rj.toString)
+        regionalJoinObjects += rj
         region += 1
       }
     }
+
     //create rdd
     val rddOmg = sc.parallelize(regionalJoinObjects).map(rj => (rj.regionNumber, rj))
+    logger.info("rddOmg count : " + rddOmg.count())
+    rddOmg.foreach(record => {
+      logger.info("region: " + record._1 + ", countT: " + record._2.tTuples.size + "countS: " + record._2.sTuples.size)
+    })
+
     // --------------------------------------
     //Try to save results to file
     try {
       //w/e -> saveAsTextFile(pathToExportResults)
-      this.logger.info("Saved theta join result tuples to : " + pathToExportResults)
+      //      this.logger.info("Saved theta join result tuples to : " + pathToExportResults)
     } catch {
       case e: Exception => this.logger.error("Couldn't save file. Message is : " + e.getMessage)
     }
-
+    logger.info("telos")
     readChar() //Pauses execution to allow for inspection
   }
 }
--- src/main/scala/RegionalJoin.scala
+++ src/main/scala/RegionalJoin.scala
@@ -3,8 +3,8 @@ import org.apache.spark.sql.Row
 case class RegionalJoin(regionNumber: Int, tTuples: List[Row], sTuples: List[Row]) {
 
 
-  //  override def toString: String = "region number:" + regionNumber + ", tTuplesCount:" + tTuples.count() +
-  //    ", sTuplesCount:" + sTuples.count()
+  override def toString: String = "region number:" + regionNumber + ", tTuplesCount:" + tTuples.size +
+    ", sTuplesCount:" + sTuples.size
 
   //Todo: ask: xreiazetai na override thn equals kai hashcode?
 }
